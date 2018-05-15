04e12ff001d5a617f543cd12151255b1808e5113
Paul Whittemore <dev@appurist.com>
2018-05-15 15:39:03 -0300

    Simpler repro.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,33 +1,35 @@
 <template>
-  <div id="app">
-    <v-app dark>
-      <v-toolbar dense fixed app color="primary">
-        <v-spacer class="hidden-sm-and-down"></v-spacer>
-        <v-toolbar-title class="hidden-sm-and-down" v-text="title"></v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn>
-          <v-icon>invert_colors</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout row wrap justify-center id="wrapper">
-            <v-flex xs10 class="mt-3">
-              <v-card>
-                <v-card-text>
-                  <p>Hello World.</p>
-                </v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-      <v-footer app>
-        <v-spacer></v-spacer>
-        <span>&copy; 2018</span>
-      </v-footer>
-    </v-app>
-  </div>
+  <v-app dark>
+    <v-toolbar fixed app>
+      <v-spacer class="hidden-sm-and-down"></v-spacer>
+      <v-toolbar-title class="hidden-sm-and-down" v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>
+        <v-icon>invert_colors</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout row wrap justify-center id="wrapper">
+          <v-flex xs10 class="mt-3">
+            <v-card>
+              <v-card-text>
+                <p>Hello World.</p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <v-spacer></v-spacer>
+      <span>&copy; 2018</span>
+    </v-footer>
+    <!--
+    -->
+  </v-app>
 </template>
 
 <script>
