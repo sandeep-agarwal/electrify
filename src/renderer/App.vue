d9dec9564a65858950259b40104c7524cecbd2b5
Paul Whittemore <dev@appurist.com>
2018-05-15 12:14:27 -0300

    Simplified version from vuetify / electron boilerplate.

---
+++ src/renderer/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <v-app dark>
+      <v-content>
+        <v-container fluid fill-height>
+          <welcome-view></welcome-view>
+        </v-container>
+      </v-content>
+      <v-navigation-drawer
+        temporary
+        fixed
+        :right="right"
+        v-model="rightDrawer"
+        app
+      >
+        <v-list>
+          <v-list-tile @click.native="right = !right">
+            <v-list-tile-action>
+              <v-icon light>compare_arrows</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-footer :fixed="fixed" app>
+        <v-spacer></v-spacer>
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  import WelcomeView from '@/components/WelcomeView'
+  export default {
+    name: 'electify',
+    components: {
+      WelcomeView
+    },
+    data: () => ({
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    })
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
+  /* Global CSS */
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
+import App from './App'
+
+Vue.use(Vuetify)
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  template: '<App/>'
+}).$mount('#app')
